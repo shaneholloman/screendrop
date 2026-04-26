7eda21956e404a841fa38ba68eb65d942fef6846
Fayaz Ahmed <fayazara@gmail.com>
2026-04-26 09:09:08 +0530

    Initial Commit

---
+++ OpenShot/OpenShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenShotApp.swift
+//  OpenShot
+//
+//  Created by Fayaz Ahmed Aralikatti on 26/04/26.
+//
+
+import SwiftUI
+
+@main
+struct OpenShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
